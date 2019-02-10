5dec556dd80d3e54838db2ee7d1434351d45b61a
sishubjoshi <skj@localhost.localdomain>
2019-02-10 05:33:19 +0530

    more

--- src/App.js
+++ src/App.js
@@ -1,21 +1,82 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import Auth from './components/Auth';
+// import Auth from './components/Auth';
 import User from './components/User';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import firebase from "firebase";
 
 import './App.css';
 
 class App extends Component {
+
+
+  componentDidMount(){
+    const config = {
+      apiKey: "AIzaSyBl28wfLspLPjiiJ02VrYbzeyuh-z9NC9g",
+      authDomain: "ccpind.firebaseapp.com",
+      projectId: "ccpind",
+      messagingSenderId: "701591541780"
+    };
+    firebase.initializeApp(config);
+  }
+  state={
+    user:null,
+    token:null,
+    isAuthenticated:false
+  }
+
+  
+
+  authenticate(e){
+    e.preventDefault();
+    let token, user;
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithPopup(provider).then((result) => {
+      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      token = result.credential.accessToken;
+      // The signed-in user info.
+      user = result.additionalUserInfo.username;
+      console.log(user);
+
+      this.setState({token,user,isAuthenticated:true});
+      <Redirect to="dashboard"/>
+      // ...
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+      alert(errorMessage)
+    });
+  }
+
+  unauthenticate(e){
+    e.preventDefault();
+    firebase.auth().signOut().then(function() {
+      this.setState({name:'',token:'',isAuthenticated:false});
+      alert("Signed Out Successful");
+    }).catch(function(error) {
+      alert(error)
+    });
+  }
 
   
   render() {
     return (
       <div className="App">
         <Navbar />
+        <h1>GitHub Authentication</h1>
+        <button type="submit" onClick={(e)=> this.authenticate(e)}>LogIn</button>
+        <button type="submit" onClick={(e)=>{this.unauthenticate(e)}}>LogOut</button>
+
       <Switch>
-        <Route exact path='/' component={Auth}/>
-        <Route path='/dashboard' component={User} />
+        {/* <Route exact path='/' component={Auth}/> */}
+        <Route path='/dashboard/:id' component={User} />
       </Switch>
           
       </div>
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,62 +1,12 @@
 import React, { Component } from 'react'
-import firebase from "firebase";
 
 export default class Auth extends Component {
 
-  state={
-    user:null,
-    token:null
-  }
-
-  componentDidMount(){
-    const config = {
-      apiKey: "AIzaSyBl28wfLspLPjiiJ02VrYbzeyuh-z9NC9g",
-      authDomain: "ccpind.firebaseapp.com",
-      projectId: "ccpind",
-      messagingSenderId: "701591541780"
-    };
-    firebase.initializeApp(config);
-  }
-
-  authenticate(e){
-    e.preventDefault();
-
-    var provider = new firebase.auth.GithubAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-      alert(errorMessage)
-    });
-  }
-
-  unauthenticate(e){
-    e.preventDefault();
-    firebase.auth().signOut().then(function() {
-      alert("Signed Out Successful");
-    }).catch(function(error) {
-      alert(error)
-    });
-  }
-
+  
   render() {
     return (
       <div>
-        <h1>GitHub Authentication</h1>
-        <button type="submit" onClick={(e)=> this.authenticate(e)}>LogIn</button>
-        <button type="submit" onClick={(e)=>{this.unauthenticate(e)}}>LogOut</button>
-      </div>
+             </div>
     )
   }
 }
