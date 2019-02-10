7eddd9c0cb8db85ca1135256aaf12197a559d60c
YITENDRA KUMAR <kumar.yitendra@gmail.com>
2019-02-10 09:42:41 +0530

    :train: edited some css

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -4,12 +4,12 @@ export default function Sidenav() {
   return (
     <div>
       <ul className="navbar-nav mt-4 side-menu">
-                        <li className="nav-items p-2">
-                            <Link to="/posts">posts</Link>
+                        <li className="nav-items p-2 slist">
+                            <Link to="/posts">Posts</Link>
                         </li>
-                        <li className="nav-items p-2" ><Link to="/myposts">my posts</Link></li>
-                        <li className="nav-items p-2"><Link to="/profile">profile</Link></li>
-                        <li className="nav-items p-2"><Link to="/profile">logout</Link></li>
+                        <li className="nav-items p-2 slist " ><Link to="/myposts">My Posts</Link></li>
+                        <li className="nav-items p-2 slist"><Link to="/profile">Profile</Link></li>
+                        <li className="nav-items p-2 slist"><Link to="/profile">Logout</Link></li>
                     </ul>
     </div>
   )
